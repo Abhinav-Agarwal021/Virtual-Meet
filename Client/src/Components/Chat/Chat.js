7aadef16b5ee586ce65e694a027ede0a8b725752
Abhinav Agarwal <agarwalabhinav309@gmail.com>
2022-01-23 04:19:19 +0530

    started video call one to one

--- Client/src/Components/Chat/Chat.js
+++ Client/src/Components/Chat/Chat.js
@@ -1,32 +1,31 @@
-import React, { useState, useEffect, useRef } from 'react'
-import { useSelector } from 'react-redux'
-import { getCsBId, getMs, getUs, sendMssgs } from '../../http/Http'
-import { Message } from '../../Shared Components/Messages/Message'
-import Picker from 'emoji-picker-react';
-import { io } from "socket.io-client"
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector } from "react-redux";
+import { getCsBId, getMs, getUs, sendMssgs } from "../../http/Http";
+import { Message } from "../../Shared Components/Messages/Message";
+import Picker from "emoji-picker-react";
+import { io } from "socket.io-client";
 import Peer from "simple-peer";
-import styles from "./Chat.module.css"
-import { useHistory } from 'react-router-dom';
+import styles from "./Chat.module.css";
+import { useHistory } from "react-router-dom";
 
 import { GoSmiley } from "react-icons/go";
 import { BiPhoneCall } from "react-icons/bi";
 import { MdKeyboardBackspace } from "react-icons/md";
-import Styles from '../one_to_one_video_Chat/VideoChat.module.css'
+import Styles from "../one_to_one_video_Chat/VideoChat.module.css";
 
 export const Chat = () => {
-
     var url = window.location.pathname;
-    var id = url.substring(url.lastIndexOf('/') + 1);
+    var id = url.substring(url.lastIndexOf("/") + 1);
 
-    const [currentChat, setCurrentChat] = useState()
-    const [friend, setfriend] = useState(null)
-    const [messages, setMessages] = useState([])
+    const [currentChat, setCurrentChat] = useState();
+    const [friend, setfriend] = useState(null);
+    const [messages, setMessages] = useState([]);
     const socket = useRef();
-    const [arrivalMessage, setArrivalMessage] = useState(null)
-    const scrollRef = useRef()
+    const [arrivalMessage, setArrivalMessage] = useState(null);
+    const scrollRef = useRef();
 
-    const [newMssg, setNewMssg] = useState("")
-    const [emojisOpen, setEmojisOpen] = useState(false)
+    const [newMssg, setNewMssg] = useState("");
+    const [emojisOpen, setEmojisOpen] = useState(false);
 
     const { user } = useSelector((state) => state.user);
     const history = useHistory();
@@ -40,44 +39,42 @@ export const Chat = () => {
     const userVideo = useRef();
     const connectionRef = useRef();
 
-    const [stream, setStream] = useState(null)
-    const [me, setMe] = useState('')
-    const [call, setCall] = useState({})
-    const [callAnswered, setCallAnswered] = useState(false)
-    const [callEnded, setCallEnded] = useState(false)
+    const [stream, setStream] = useState(null);
+    const [me, setMe] = useState("");
+    const [call, setCall] = useState({});
+    const [callAnswered, setCallAnswered] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
 
     useEffect(() => {
         socket.current = io("ws://localhost:8900");
-        navigator.mediaDevices.getUserMedia({
-            audio: true,
-            video: {
-                width: { min: 1024, ideal: 1280, max: 1920 },
-                height: { min: 576, ideal: 720, max: 1080 }
-            }
-        })
+
+        navigator.mediaDevices
+            .getUserMedia({
+                audio: true,
+                video: true,
+            })
             .then((currentStream) => {
                 setStream(currentStream);
-                console.log(currentStream)
                 myVideo.current.srcObject = currentStream;
-            })
+            });
 
-        socket.current.on('me', (id) => setMe(id));
+        socket.current.on("me", (id) => setMe(id));
 
-        socket.current.on('callfriend', ({ from, signal }) => {
-            setCall({ isReceivedCall: true, from, signal });
-        })
-    }, [])
+        socket.current.on("callfriend", ({ from, name: callerName, signal }) => {
+            setCall({ isReceivedCall: true, from, name: callerName, signal });
+        });
+    }, []);
 
     const answerCall = () => {
         setCallAnswered(true);
 
         const peer = new Peer({ initiator: false, trickle: false, stream });
 
-        peer.on('signal', (data) => {
-            socket.current.emit('callanswered', { signal: data, to: call.from });
+        peer.on("signal", (data) => {
+            socket.current.emit("callanswered", { signal: data, to: call.from });
         });
 
-        peer.on('stream', (currentStream) => {
+        peer.on("stream", (currentStream) => {
             userVideo.current.srcObject = currentStream;
         });
 
@@ -89,15 +86,19 @@ export const Chat = () => {
     const callUser = (id) => {
         const peer = new Peer({ initiator: true, trickle: false, stream });
 
-        peer.on('signal', (data) => {
-            socket.current.emit('callfriend', { userToCall: id, signalData: data, from: me });
+        peer.on("signal", (data) => {
+            socket.current.emit("callfriend", {
+                userToCall: id,
+                signalData: data,
+                from: me,
+            });
         });
 
-        peer.on('stream', (currentStream) => {
+        peer.on("stream", (currentStream) => {
             userVideo.current.srcObject = currentStream;
         });
 
-        socket.current.on('callanswered', (signal) => {
+        socket.current.on("callanswered", (signal) => {
             setCallAnswered(true);
 
             peer.signal(signal);
@@ -108,7 +109,6 @@ export const Chat = () => {
 
     const leaveCall = () => {
         setCallEnded(true);
-
         connectionRef.current.destroy();
     };
 
@@ -130,17 +130,17 @@ export const Chat = () => {
     }, [arrivalMessage, currentChat]);
 
     useEffect(() => {
-        const chat = currentChat?.members.find((m) => m !== user.id)
+        const chat = currentChat?.members.find((m) => m !== user.id);
 
         if (chat) {
             const getFrined = async () => {
                 const Friend = await getUs(chat);
                 setfriend(Friend.data);
-            }
+            };
 
             getFrined();
         }
-    }, [user, currentChat])
+    }, [user, currentChat]);
 
     useEffect(() => {
         socket.current.emit("addUser", user.id);
@@ -149,39 +149,38 @@ export const Chat = () => {
     useEffect(() => {
         const getConversation = async () => {
             try {
-                const res = await getCsBId(id)
-                setCurrentChat(res.data)
+                const res = await getCsBId(id);
+                setCurrentChat(res.data);
             } catch (error) {
-                console.log(error)
+                console.log(error);
             }
-        }
+        };
 
-        getConversation()
-    }, [id])
+        getConversation();
+    }, [id]);
 
     useEffect(() => {
         if (currentChat) {
             const getMessages = async () => {
                 try {
-                    const res = await getMs(currentChat?._id)
+                    const res = await getMs(currentChat?._id);
                     setMessages(res.data);
                 } catch (error) {
-                    console.log(error)
+                    console.log(error);
                 }
-            }
-            getMessages()
+            };
+            getMessages();
         }
-    }, [currentChat])
+    }, [currentChat]);
 
     const sendMssg = async () => {
-
         if (!newMssg) return;
         const userCs = {
             sender: user.id,
             senderName: user.name,
             message: newMssg,
-            conversationId: currentChat?._id
-        }
+            conversationId: currentChat?._id,
+        };
 
         const receiverId = currentChat?.members.find(
             (member) => member !== user.id
@@ -196,59 +195,72 @@ export const Chat = () => {
 
         try {
             const res = await sendMssgs(userCs);
-            setNewMssg("")
-            setMessages([...messages, res.data])
+            setNewMssg("");
+            setMessages([...messages, res.data]);
         } catch (error) {
-            console.log("message not sent")
+            console.log("message not sent");
         }
-    }
+    };
 
     const handleKeyDown = (e) => {
-        if (e.key === 'Enter') {
+        if (e.key === "Enter") {
             sendMssg();
         }
-    }
+    };
 
     useEffect(() => {
         scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-    }, [messages])
+    }, [messages]);
 
     const handleBack = () => {
         history.goBack();
-    }
+    };
 
     const handleCallUser = () => {
-        callUser(friend?.id);
-    }
+        callUser(friend?._id);
+    };
 
     return (
         <div className={styles.messenger}>
             <div className={styles.chat__Box}>
                 <div className={styles.chat__wrapper}>
                     <div className={styles.dm__navbar}>
-                        <span className={styles.go__back} onClick={handleBack} >
+                        <span className={styles.go__back} onClick={handleBack}>
                             <MdKeyboardBackspace className={styles.go__back__icon} />
                         </span>
-                        <div className={styles.friend}>
-                            @ {friend?.name}
-                        </div>
-                        <BiPhoneCall className={styles.phone__call} onClick={handleCallUser} />
+                        <div className={styles.friend}>@ {friend?.name}</div>
+                        <BiPhoneCall
+                            className={styles.phone__call}
+                            onClick={handleCallUser}
+                        />
                     </div>
-                    {/*{
-                            (callAnswered && !callEnded) ?
-                                <div className={Styles.video__containers}>
-                                    <video className={Styles.video} muted ref={userVideo} autoPlay playsInline />
-                                     <button onClick={leaveCall}>End call</button>
-                                </div>
-                                :
-                                (call.isReceivedCall && !callAnswered) ?
-                                    <div className={Styles.calling}>
-                                        <h1>{user.name} is calling....</h1>
-                                        <button onClick={answerCall}>accept call</button>
-                                    </div>
-                                    : */}
+                    {callAnswered && !callEnded && (
+                        <div className={Styles.video__containers}>
+                            <video
+                                className={Styles.video}
+                                muted
+                                ref={userVideo}
+                                autoPlay
+                                playsInline
+                            />
+                            <button onClick={leaveCall}>End call</button>
+                        </div>
+                    )}
+                    {call.isReceivedCall && !callAnswered && (
+                        <div className={Styles.calling}>
+                            {console.log("user is calling")}
+                            <h1>{user.name} is calling....</h1>
+                            <button onClick={answerCall}>accept call</button>
+                        </div>
+                    )}
                     <div className={Styles.me__calling}>
-                        <video className={Styles.video} muted ref={myVideo} autoPlay playsInline />
+                        <video
+                            className={Styles.video}
+                            muted
+                            ref={myVideo}
+                            autoPlay
+                            playsInline
+                        />
                     </div>
                     <div className={styles.chatBox__top}>
                         {messages.map((msg) => (
@@ -257,18 +269,39 @@ export const Chat = () => {
                             </div>
                         ))}
                     </div>
-                    {emojisOpen &&
+                    {emojisOpen && (
                         <div className={styles.emojis}>
-                            <Picker onEmojiClick={onEmojiClick} disableAutoFocus disableSkinTonePicker />
+                            <Picker
+                                onEmojiClick={onEmojiClick}
+                                disableAutoFocus
+                                disableSkinTonePicker
+                            />
                         </div>
-                    }
+                    )}
                     <div className={styles.send__chat}>
-                        <GoSmiley className={styles.emoji__selection} onClick={() => setEmojisOpen(!emojisOpen)} />
-                        <input value={newMssg} onClick={() => setEmojisOpen(false)} className={styles.write__mssg} autoFocus="autoFocus" type="message" placeholder={`Message @${friend?.name}`} onChange={(e) => setNewMssg(e.target.value)} onKeyDown={handleKeyDown} />
-                        <img className={styles.send__mssg} src="/images/send-icon.png" alt="" onClick={sendMssg} />
+                        <GoSmiley
+                            className={styles.emoji__selection}
+                            onClick={() => setEmojisOpen(!emojisOpen)}
+                        />
+                        <input
+                            value={newMssg}
+                            onClick={() => setEmojisOpen(false)}
+                            className={styles.write__mssg}
+                            autoFocus="autoFocus"
+                            type="message"
+                            placeholder={`Message @${friend?.name}`}
+                            onChange={(e) => setNewMssg(e.target.value)}
+                            onKeyDown={handleKeyDown}
+                        />
+                        <img
+                            className={styles.send__mssg}
+                            src="/images/send-icon.png"
+                            alt=""
+                            onClick={sendMssg}
+                        />
                     </div>
                 </div>
             </div>
         </div>
-    )
-}
+    );
+};
--- Socket/Socket.js
+++ Socket/Socket.js
@@ -1,8 +1,8 @@
-const io = require('socket.io')(8900, {
-    cors: {
-        origin: "http://localhost:3000"
-    }
-})
+const io = require("socket.io")(8900, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
 
 let users = [];
 
@@ -10,79 +10,88 @@ const peerUsers = {};
 const socketToRoom = {};
 
 const addUser = (userId, socketId) => {
-    !users.some((user) => user.userId === userId) &&
-        users.push({ userId, socketId });
+  !users.some((user) => user.userId === userId) &&
+    users.push({ userId, socketId });
 };
 
 const removeUser = (socketId) => {
-    users = users.filter((user) => user.socketId !== socketId);
+  users = users.filter((user) => user.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-    return users.find((user) => user.userId === userId);
+  return users.find((user) => user.userId === userId);
 };
 
 io.on("connection", (socket) => {
-    console.log("user is connected...")
+  console.log("user is connected...");
 
-    socket.emit("me", socket.id)
+  socket.emit("me", socket.id);
 
-    socket.on("addUser", (userId) => {
-        addUser(userId, socket.id);
-        io.emit("getUsers", users);
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  socket.on("sendMessage", ({ senderId, senderName, message }) => {
+    io.local.emit("getMessage", {
+      senderId,
+      senderName,
+      message,
     });
+  });
 
-    socket.on("sendMessage", ({ senderId, senderName, message }) => {
-        io.local.emit("getMessage", {
-            senderId,
-            senderName,
-            message,
-        });
+  //for individual call
+  socket.on("callfriend", ({ userToCall, signalData, from }) => {
+    const friend = getUser(userToCall);
+    io.to(friend.socketId).emit("callfriend", {
+      signal: signalData,
+      from,
     });
+  });
 
-    //for individual call
-    socket.on("callfriend", ({ userToCall,name, signalData, from }) => {
-        io.to(userToCall).emit("callfriend",{signal:signalData,from,name})
-    })
+  socket.on("callanswered", (data) => {
+    io.to(data.to).emit("callanswered", data.signal);
+  });
 
-    socket.on("callanswered", (data) => {
-        io.to(data.to).emit("callanswered", data.signal);
-    })
+  //for video chat with multiple people
+  socket.on("join room", (roomId) => {
+    if (peerUsers[roomId]) {
+      const length = peerUsers[roomId].length;
+      if (length === 10) {
+        socket.emit("room is full");
+        return;
+      }
+      if (!peerUsers[roomId].includes(socket.id)) {
+        peerUsers[roomId].push(socket.id);
+      }
+    } else {
+      peerUsers[roomId] = [socket.id];
+    }
 
+    socketToRoom[socket.id] = roomId;
+    const usersinThisRoom = peerUsers[roomId].filter((id) => id != socket.id);
 
-    //for video chat with multiple people
-    socket.on("join room", roomId => {
-        if (peerUsers[roomId]) {
-            const length = peerUsers[roomId].length;
-            if (length === 10) {
-                socket.emit("room is full")
-                return;
-            }
-            if (!peerUsers[roomId].includes(socket.id)) {
-                peerUsers[roomId].push(socket.id)
-            }
-        } else {
-            peerUsers[roomId] = [socket.id];
-        }
+    socket.emit("all users", usersinThisRoom);
+  });
 
-        socketToRoom[socket.id] = roomId;
-        const usersinThisRoom = peerUsers[roomId].filter(id => id != socket.id)
-
-        socket.emit("all users", usersinThisRoom);
-    })
-
-    socket.on("sendSignal", data => {
-        io.to(data.userToSignal).emit('user joined', { signal: data.signal, callerId: data.callerId })
-    })
-
-    socket.on("returningSignal", data => {
-        io.to(data.callerId).emit('receiving returned signal', { signal: data.signal, id: socket.id })
-    })
-    //till here
-
-    socket.on("disconnect", () => {
-        console.log("a user disconnected!");
-        removeUser(socket.id);
-        io.emit("getUsers", users);
+  socket.on("sendSignal", (data) => {
+    io.to(data.userToSignal).emit("user joined", {
+      signal: data.signal,
+      callerId: data.callerId,
     });
-})
+  });
+
+  socket.on("returningSignal", (data) => {
+    io.to(data.callerId).emit("receiving returned signal", {
+      signal: data.signal,
+      id: socket.id,
+    });
+  });
+  //till here
+
+  socket.on("disconnect", () => {
+    console.log("a user disconnected!");
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
