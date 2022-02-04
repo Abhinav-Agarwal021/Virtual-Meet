f2804b8abe343fb10e364c264ca7346d6a4e690d
Abhinav Agarwal <agarwalabhinav309@gmail.com>
2022-02-05 03:43:37 +0530

    role settings done

--- Client/src/Components/GrpSettings/GrpSettings.js
+++ Client/src/Components/GrpSettings/GrpSettings.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { getRId, updateServerName } from '../../http/Http';
+import { getRId, getRoom, updateServerName } from '../../http/Http';
 import styles from './GrpSettings.module.css'
 
 export const GrpSettings = () => {
@@ -7,8 +7,9 @@ export const GrpSettings = () => {
     var url = window.location.pathname;
     var id = url.substring(url.lastIndexOf('/') + 1);
 
-    const [selected, setSelected] = useState(0);
+    const [selected, setSelected] = useState(1);
     const [serverName, setServerName] = useState(null);
+    const [categories, setCategories] = useState([])
 
     const settings = [
         "overview",
@@ -48,6 +49,19 @@ export const GrpSettings = () => {
         updatename();
     }
 
+    useEffect(() => {
+        const getRoomData = async () => {
+            const room = await getRoom(id);
+            setCategories(room.data);
+        }
+
+        getRoomData();
+    }, [id])
+
+    const handleDeleteRole = () => {
+        
+    }
+
     return (
         <div className={styles.server__Settings}>
             <div className={styles.server__menu}>
@@ -63,20 +77,53 @@ export const GrpSettings = () => {
                     </div>
                 </div>
             </div>
-            <div className={styles.content}>
-                <div className={styles.set__content}>
-                    <div className={styles.set__header}>
-                        <p className={styles.sett__name}>Server Overview</p>
-                    </div>
-                    <div className={styles.set__1}>
-                        <p>SERVER NAME</p>
-                        <input type="text" value={serverName} onChange={(e) => setServerName(e.target.value)} />
-                    </div>
-                    <div className={styles.set__footer}>
-                        <button onClick={handleupdateserver}>Apply</button>
+            {selected === 0 &&
+                <div className={styles.content}>
+                    <div className={styles.set__content}>
+                        <div className={styles.set__header}>
+                            <p className={styles.sett__name}>Server Overview</p>
+                        </div>
+                        <div className={styles.set__1}>
+                            <p>SERVER NAME</p>
+                            <input type="text" value={serverName} onChange={(e) => setServerName(e.target.value)} />
+                        </div>
+                        <div className={styles.set__footer}>
+                            <button onClick={handleupdateserver}>Apply</button>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
+            {selected === 1 &&
+                <div className={styles.content}>
+                    <div className={styles.set__content}>
+                        <div className={styles.set__header}>
+                            <p className={styles.sett__name}>Roles</p>
+                            <p className={styles.set__desc}>use roles to organize your server members and customize their permissions.</p>
+                        </div>
+                        <div className={styles.create__btn}>
+                            <button>Create Role</button>
+                        </div>
+                        <div className={styles.set__header}>
+                            <p className={styles.set__desc}># Deleting a role will make that category public and open to everyone</p>
+                        </div>
+
+                        <div className={styles.set__display}>
+                            <p className={styles.role}>ROLES</p>
+                            <p className={styles.mem}>MEMBERS</p>
+                            <p className={styles.cat}>CATAGORY</p>
+                            <p className={styles.btn}></p>
+                        </div>
+                        {categories.map((catdet, idx) => (
+                            <div key={idx} className={styles.role__data}>
+                                <p className={styles.role}>{catdet.role}</p>
+                                <p className={styles.mem}>0</p>
+                                <p className={styles.cat}>{catdet.name}</p>
+                                <p className={`${styles.btn} ${styles.del__role}`} onClick={handleDeleteRole}>Delete Role</p>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            }
         </div>
     );
 };
